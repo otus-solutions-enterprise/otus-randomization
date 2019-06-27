5dbd53332088418024aef217d26e27fbf2e00838
Tiago Matana <tiago.matana@gmail.com>
2019-06-27 11:21:34 -0300

    OR-14 Refatoração para response e adição de editorconfig

--- nodejs/api/app.js
+++ nodejs/api/app.js
@@ -3,26 +3,26 @@ const mongoose = require("mongoose");
 const listEndpoints = require('express-list-endpoints');
 
 const {
-    MONGO_USERNAME,
-    MONGO_PASSWORD,
-    MONGO_HOSTNAME,
-    MONGO_PORT,
-    MONGO_DB,
-    API_PORT
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_HOSTNAME,
+  MONGO_PORT,
+  MONGO_DB,
+  API_PORT
 } = process.env;
 
 const port = API_PORT || 8080;
 
 const options = {
-    useNewUrlParser: true,
-    reconnectTries: Number.MAX_VALUE,
-    reconnectInterval: 500,
-    connectTimeoutMS: 10000,
-    keepAlive: 1,
-    auth: {
-        user: MONGO_USERNAME || "root",
-        password: MONGO_PASSWORD || "XRYs9yjU"
-    }
+  useNewUrlParser: true,
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 500,
+  connectTimeoutMS: 10000,
+  keepAlive: 1,
+  auth: {
+    user: MONGO_USERNAME || "root",
+    password: MONGO_PASSWORD || "XRYs9yjU"
+  }
 };
 
 const url = `mongodb://${MONGO_HOSTNAME || "localhost"}:${MONGO_PORT || "27017"}/${MONGO_DB || "otus-randomization"}?authSource=admin`;
@@ -30,22 +30,22 @@ const url = `mongodb://${MONGO_HOSTNAME || "localhost"}:${MONGO_PORT || "27017"}
 connect();
 
 function listen() {
-    app.listen(port);
-    console.log('Express app started on port ' + port);
+  app.listen(port);
+  console.log('Express app started on port ' + port);
 }
 
 function connect() {
-    mongoose.connection
-        .on('error', console.log)
-        .on('disconnected', connect)
-        .once('open', listen);
-    endpointsList();
-    return mongoose.connect(url, options);
+  mongoose.connection
+    .on('error', console.log)
+    .on('disconnected', connect)
+    .once('open', listen);
+  endpointsList();
+  return mongoose.connect(url, options);
 }
 
-function endpointsList(){
-    let endpoints = listEndpoints(app);
-    console.table(endpoints)
+function endpointsList() {
+  let endpoints = listEndpoints(app);
+  console.table(endpoints)
 }
 
 
--- nodejs/api/app/routes/RandomizationRest.js
+++ nodejs/api/app/routes/RandomizationRest.js
@@ -1,60 +1,60 @@
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 
-module.exports = function(application) {
-    application.post('/api/create-table',jsonParser, function(req, res) {
-        res.header('Content-Type', 'application/json');
-        let participants = req.body.participants;
-        let blocSize = req.body.blocSize;
-        let groups = req.body.groups;
-        let projectName = req.body.projectName;
+module.exports = function (application) {
+  application.post('/api/create-table', jsonParser, function (req, res) {
+    res.header('Content-Type', 'application/json');
+    let participants = req.body.participants;
+    let blocSize = req.body.blocSize;
+    let groups = req.body.groups;
+    let projectName = req.body.projectName;
 
-        if(!participants || !blocSize || !groups){
-            res.status(406).send({data:"Invalid Fields"})
-        }
+    if (!participants || !blocSize || !groups) {
+      res.status(406).send({data: "Invalid Fields"})
+    }
 
-        application.app.controllers.RandomizationController.createTable(projectName, participants, blocSize, groups)
-        .then(result=>{
-            res.status(result.code).send(result.body)
-        })
-        .catch(err =>{
-            res.status(err.code).send(err.body)
-        });
-    });
+    application.app.controllers.RandomizationController.createTable(projectName, participants, blocSize, groups)
+      .then(result => {
+        res.status(result.code).send(result.body)
+      })
+      .catch(err => {
+        res.status(err.code).send(err.body)
+      });
+  });
 
-    application.post('/api/randomize',jsonParser, function(req, res) {
-        res.header('Content-Type', 'application/json');
-        let elementId = req.body.elementId;
-        let tableId = req.body.tableId;
+  application.post('/api/randomize', jsonParser, function (req, res) {
+    res.header('Content-Type', 'application/json');
+    let elementId = req.body.elementId;
+    let tableId = req.body.tableId;
 
-        if(!elementId || !tableId){
-            res.status(406).send({data:"Invalid Fields"})
-        }
+    if (!elementId || !tableId) {
+      res.status(406).send({data: "Invalid Fields"})
+    }
 
-        application.app.controllers.RandomizationController.randomizeElement(elementId, tableId)
-            .then(result=>{
-                res.status(result.code).send(result.body)
-            })
-            .catch(err =>{
-                res.status(err.code).send(err.body)
-            });
-    });
+    application.app.controllers.RandomizationController.randomizeElement(elementId, tableId)
+      .then(result => {
+        res.status(result.code).send(result.body)
+      })
+      .catch(err => {
+        res.status(err.code).send(err.body)
+      });
+  });
 
-    application.post('/api/participant-group',jsonParser, function(req, res) {
-        res.header('Content-Type', 'application/json');
-        let elementId = req.body.elementId;
-        let tableId = req.body.tableId;
+  application.post('/api/participant-group', jsonParser, function (req, res) {
+    res.header('Content-Type', 'application/json');
+    let elementId = req.body.elementId;
+    let tableId = req.body.tableId;
 
-        if(!elementId || !tableId){
-            res.status(406).send({data:"Invalid Fields"})
-        }
+    if (!elementId || !tableId) {
+      res.status(406).send({data: "Invalid Fields"})
+    }
 
-        application.app.controllers.RandomizationController.getGroupParticipant(elementId, tableId)
-            .then(result=>{
-                res.status(result.code).send(result.body)
-            })
-            .catch(err =>{
-                res.status(err.code).send(err.body)
-            });
-    });
+    application.app.controllers.RandomizationController.getGroupParticipant(elementId, tableId)
+      .then(result => {
+        res.status(result.code).send(result.body)
+      })
+      .catch(err => {
+        res.status(err.code).send(err.body)
+      });
+  });
 };
